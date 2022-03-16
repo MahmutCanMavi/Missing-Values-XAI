fad787e2a852c32bb9340f9ba73401825de40a60
Michael Zellinger <michael.zellinger@stat.math.ethz.ch>
2022-03-16 17:59:38 +0100

    implement classes for FeatureInfo and PctAvail

---
+++ react-frontend/src/types/FeatureInfo.ts
@@ -0,0 +1,6 @@
+import { PctAvail } from "./PctAvail";
+
+export interface FeatureInfo {
+    feature_name: string;
+    pct_avail_pp: PctAvail[];
+}
---
+++ react-frontend/src/types/PctAvail.ts
@@ -0,0 +1,4 @@
+export interface PctAvail {
+    patient_id: number; // patient ID as an integer
+    pct_avail: number; // percent of available values as a fraction (between 0 and 1)
+}
