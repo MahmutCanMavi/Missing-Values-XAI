3ef5b8705bed3a723bcd9afd14b657048fa7b3fd
Talu <tkaragoez@student.ethz.ch>
2022-06-04 21:51:38 +0200

    String Imputations, changed data source to new

--- backend-project/impute.py
+++ backend-project/impute.py
@@ -54,7 +54,7 @@ class Imputation_Method():
                     continue
 
             
-            error[self.features[i]] = sqrt(error[self.features[i]] / len(to_be_nand[i])) / data[self.features[i]].mean()
+            error[self.features[i]] = sqrt(error[self.features[i]] / len(to_be_nand[i])) / abs(data[self.features[i]].mean())
         return error, imputed
                 
 class Forward_Fill(Imputation_Method):
@@ -66,7 +66,6 @@ class Forward_Fill(Imputation_Method):
         out = pd.DataFrame(columns=self.features)
         for patient in data["id"].unique(): 
             out = pd.concat([out, data.loc[data.id == patient][self.features].ffill().fillna(data[self.features].mean())])
-        print(out)
         return out, self.features
 
 class Value_Fill(Imputation_Method):
@@ -117,8 +116,85 @@ class Iterative_Impute(Imputation_Method):
         return pd.DataFrame(imputed_data, columns = self.imputable_features), self.imputable_features
         
 
+class String_Impute(Imputation_Method):
+    def __init__(self, features: list[str]):
+        super().__init__(features, True)
+
+    def encoding(self, data : pd.DataFrame):
+        self.encoder, self.decoder = {}, {}
+        
+        for feature in self.features:
+            self.encoder[feature], self.decoder[feature] = {}, {}
+            ctr = 0
+            for value in data[feature].unique():
+                if type(value) != str: continue
+                self.encoder[feature][value] = ctr
+                self.decoder[feature][ctr] = value
+                ctr += 1
+    
+    def encode(self, data : pd.DataFrame):
+        self.encoding(data)
+        for feature in self.features:
+            data[feature].replace(self.encoder[feature], inplace = True)
+        
+        return data
+    
+    def decode(self, data : pd.DataFrame):
+        for feature in self.features:
+            data[feature].replace(self.decoder[feature], inplace = True)
+        
+        return data
+    
+    def impute(self, data : pd.DataFrame, no_decode = False):
+        data = self.encode(data)
+        out = pd.DataFrame(columns=self.features)
+        for patient in data["id"].unique(): 
+            out = pd.concat([out, data.loc[data.id == patient][self.features].ffill().fillna(data[self.features].mean())]).astype(int)
+        if not no_decode:
+            return self.decode(out)[self.features], self.features
+        else:
+            return out, self.features
+    
+    
+    def evaluate_imputation(self, data : pd.DataFrame):
+        # copy data, so nothing happens to it
+        # maybe not necessary in hindsight
+        new_df = data[self.features].copy(deep=True)
+        if self.need_ids: new_df["id"] = data.id
+        # list is for holding the random samples
+        # dict is for holding errors
+        to_be_nand, error = [], {}
+        for feature in self.features:
+            # samples 10% of the non NaN data at random (barring seed)
+            to_be_nand.append(sample([i for i, x in enumerate(~data[feature].isna()) if x], 
+                                     int(len([i for i, x in enumerate(~data[feature].isna()) if x]) * 0.01)))
+            # those random data are NaNd
+            for bye in to_be_nand[-1]:
+                new_df[feature].loc[bye] = np.NaN
+        # impute the data
+        imputed, _ = self.impute(new_df, no_decode = True)
+        data = self.encode(data)
+        for i in range(len(self.features)):
+            print(data[self.features[i]].isna().sum(), imputed[self.features[i]].isna().sum())
+            error[self.features[i]] = 0
+            for gone in to_be_nand[i]:
+                error[self.features[i]] += abs(data[self.features[i]].loc[gone] - imputed[self.features[i]].loc[gone])
+            
+            error[self.features[i]] = sqrt(error[self.features[i]] / len(to_be_nand[i]))
+        return error, self.decode(imputed)
+
+
 # Function to call from the outside
 def errors_e2e(features: list[str], method_name : str, method_parameters):
+    data = pd.read_csv(app.DATA_PATH)
+    is_string = False
+    # Those pesky strings, hard to impute
+    string_features = data.select_dtypes(object).columns.to_list()
+    for feature in features: 
+        if feature in string_features: 
+            method_name = "string"
+            is_string = True
+            
     # Error selector
     if method_name == "value":
         # Check Parameters
@@ -153,23 +229,25 @@ def errors_e2e(features: list[str], method_name : str, method_parameters):
             K=int(K_param["value"])
         imputer = KNN_Impute(features,K)
     elif method_name == "iterative": imputer = Iterative_Impute(features)
+    elif method_name == "string": imputer = String_Impute(features)
     elif method_name == "None": return None
     else:
         print("Error: Imputation method " + str(method_name) + " is not one of the supported imputation methods")
     
-    errors, imputed = imputer.evaluate_imputation(pd.read_csv(app.DATA_PATH))
+    errors, imputed = imputer.evaluate_imputation(data)
     ErrorInfos = []
     for feature in features:
         # comment david: I extract it in the front-end anyway, we dont neet the pct_avail data here
         pct_avail_dict = {"feature_name":feature} #pct_avail_pp(feature) 
         pct_avail_dict["imputation_error"] = errors[feature]
+        pct_avail_dict["is_string"] = is_string
         ErrorInfos.append(pct_avail_dict)
     return ErrorInfos, imputed
     
 
 if __name__ == '__main__':
     thisfile= str(pathlib.Path(__file__).parent.absolute())
-    path = thisfile+"/data/icu_data_with_na_v2.csv"
+    path = thisfile+"/data/new_data_filled_legacy.csv"
     data = pd.read_csv(path)
     
     features = ["HR", "theo_bol"]
--- backend-project/test.py
+++ backend-project/test.py
@@ -5,8 +5,8 @@ imp_method_ffill = {"name": "ffill", "parameters": None}
 imp_method_mean = {"name": "mean", "parameters": None}
 imp_method_value = {"name": "value", "parameters": val_fill_params}
 
-featureInfos = [{"group_id": 1, "feature_name" : "HR"}, {"group_id": 1, "feature_name" : "theo_bol"}, {"group_id": 2, "feature_name" : "Na"}]
-groups = [{"id": 1, "imputation_method":imp_method_ffill}, {"id": 2, "imputation_method":imp_method_value}]
+featureInfos = [{"group_id": 1, "feature_name" : "mvent"}, {"group_id": 1, "feature_name" : "surgery"}, {"group_id": 2, "feature_name" : "Na"}]
+groups = [{"id": 1, "imputation_method":imp_method_ffill}, {"id": 2, "imputation_method":imp_method_ffill}]
 
 d = {"featureInfos": featureInfos, "groups": groups}
 
