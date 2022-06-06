044372316b072225a3c7ee7f1092c05da1f147a1
Talu <tkaragoez@student.ethz.ch>
2022-06-06 14:24:48 +0200

    Parallel imputation, more error catching

--- backend-project/test.py
+++ backend-project/test.py
@@ -1,13 +1,26 @@
 import app
-val_fill_params = {"mode": "int", "name": "value", "value" : 5}
+import pandas as pd
+import random
+import time
+
+tic = time.perf_counter()
+data = pd.read_csv(app.DATA_PATH)
+print(len(data) == data["HR"].isna().sum())
+n_clusters = 4
+cols = [x for x in data.columns.to_list() if x not in app.UNTOUCHABLE]
 
 imp_method_ffill = {"name": "ffill", "parameters": None}
 imp_method_mean = {"name": "mean", "parameters": None}
-imp_method_value = {"name": "value", "parameters": val_fill_params}
+imp_method_iterative = {"name": "iterative", "parameters": None}
 
-featureInfos = [{"group_id": 1, "feature_name" : "cancer"}, {"group_id": 1, "feature_name" : "surgery"}, {"group_id": 2, "feature_name" : "Na"}]
-groups = [{"id": 1, "imputation_method":imp_method_ffill}, {"id": 2, "imputation_method":imp_method_ffill}]
+method = [imp_method_ffill, imp_method_mean, imp_method_iterative]
+
+featureInfos = [{"group_id": random.randint(1,4), "feature_name" : feature} for feature in cols]
+groups = [{"id" : cluster, "imputation_method" : random.choice(method)} for cluster in range(1,n_clusters+1)]
 
 d = {"featureInfos": featureInfos, "groups": groups}
 
-s = app.get_imputation(d)
+s = app.get_imputations(d)
+
+toc = time.perf_counter()
+print(f"Downloaded the tutorial in {toc - tic:0.4f} seconds")
