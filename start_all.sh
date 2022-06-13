61fdb4a0b5702e519e1140fb57f3ff85fe0f251d
David Metzger <dave.m@solnet.ch>
2022-06-13 15:19:08 +0200

    gitignore start_all.sh

--- start_all.sh
+++
@@ -1,7 +0,0 @@
-# start back and frontend in one go
-# start with bash -i ./start_all.sh
-conda activate XAI
-cd backend-project
-gnome-terminal -- uvicorn app:app --reload
-cd ../react-frontend
-npm start
